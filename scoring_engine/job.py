663790d9e011dbfd6a93d631dabc675b18540810
Brandon Myers <bmyers@mozilla.com>
2016-10-26 17:13:48 -0500

    Add result field to job class
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/job.py
+++ scoring_engine/job.py
@@ -1,18 +1,27 @@
 class Job(object):
-    def __init__(self, service_id, command, output=None):
+    def __init__(self, service_id, command, output=None, result=None):
         self.service_id = service_id
         self.command = command
         self.output = output
+        self.result = result
 
     def to_dict(self):
         return {
             "service_id": self.service_id,
             "command": self.command,
-            "output": self.output
+            "output": self.output,
+            "result": self.result,
         }
+
+    def passed(self):
+        if self.result == 'Pass':
+            return True
+        return False
 
     def from_dict(in_dict):
         job = Job(service_id=in_dict['service_id'], command=in_dict['command'])
         if in_dict['output']:
             job.output = in_dict['output']
+        if in_dict['result']:
+            job.result = in_dict['result']
         return job
--- tests/scoring_engine/test_job.py
+++ tests/scoring_engine/test_job.py
@@ -12,13 +12,17 @@ class TestJob(object):
         assert job.service_id == "12345"
         assert job.command == "ls -l"
         assert job.output is None
+        assert job.result is None
+        assert job.passed() is False
 
     def test_init_with_output(self):
         cmd_output = "-rw-r--r--  1 root  root  0 Oct 26 15:56 abc"
-        job = Job(service_id="12345", command="ls -l", output=cmd_output)
+        job = Job(service_id="12345", command="ls -l", output=cmd_output, result='Pass')
         assert job.service_id == "12345"
         assert job.command == "ls -l"
         assert job.output == cmd_output
+        assert job.result == 'Pass'
+        assert job.passed() is True
 
     def test_add_output(self):
         cmd_output = "-rw-r--r--  1 root  root  0 Oct 26 15:56 abc"
@@ -26,16 +30,18 @@ class TestJob(object):
         assert job.service_id == "12345"
         assert job.command == "ls -l"
         assert job.output is None
+        assert job.result is None
         job.output = cmd_output
         assert job.output == cmd_output
 
     def test_to_dict(self):
         cmd_output = "-rw-r--r--  1 root  root  0 Oct 26 15:56 abc"
-        job = Job(service_id="12345", command="ls -l", output=cmd_output)
+        job = Job(service_id="12345", command="ls -l", output=cmd_output, result='Fail')
         expected_dict = {
             'service_id': '12345',
             'command': 'ls -l',
-            'output': '-rw-r--r--  1 root  root  0 Oct 26 15:56 abc'
+            'output': '-rw-r--r--  1 root  root  0 Oct 26 15:56 abc',
+            'result': 'Fail'
         }
         assert job.to_dict() == expected_dict
 
@@ -44,10 +50,21 @@ class TestJob(object):
         input_dict = {
             'service_id': '12345',
             'command': 'ls -l',
-            'output': cmd_output
+            'output': cmd_output,
+            'result': "Pass",
         }
         returned_job = Job.from_dict(input_dict)
         assert isinstance(returned_job, Job)
         assert returned_job.service_id == "12345"
         assert returned_job.command == "ls -l"
         assert returned_job.output == cmd_output
+        assert returned_job.result == 'Pass'
+        assert returned_job.passed() is True
+
+    def test_passed_with_fail(self):
+        job = Job(service_id="12", command="ls -l", output="Error", result='Fail')
+        assert job.passed() is False
+
+    def test_passed_with_pass(self):
+        job = Job(service_id="12", command="ls -l", output="abc", result='Pass')
+        assert job.passed() is True
