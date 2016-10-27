d21b065800ea7db80b25de6554943bc3c53fcdfa
Brandon Myers <bmyers@mozilla.com>
2016-10-27 17:56:38 -0500

    Add finished and worker queue for job queue
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

---
+++ scoring_engine/finished_queue.py
@@ -0,0 +1,7 @@
+from job_queue import JobQueue
+
+
+class FinishedQueue(JobQueue):
+    def __init__(self):
+        super(FinishedQueue, self).__init__()
+        self.queue_name = "finished"
---
+++ scoring_engine/job_queue.py
@@ -0,0 +1,39 @@
+import redis
+import json
+
+from config import Config
+from job import Job
+from malformed_job import MalformedJob
+
+
+class JobQueue(object):
+    def __init__(self):
+        config = Config()
+        self.queue_name = config.redis_queue_name
+        self.conn = redis.Redis(host=config.redis_host, port=config.redis_port, password=config.redis_password)
+        self.namespace = config.redis_namespace
+        # This is meant to get overwritten by children
+        self.queue_name = "jobs"
+
+    @property
+    def queue_key(self):
+        return self.namespace + ':' + self.queue_name
+
+    def size(self):
+        return int(self.conn.llen(self.queue_key))
+
+    def clear(self):
+        while self.size() > 0:
+            self.get_job()
+
+    def add_job(self, job):
+        if not isinstance(job, Job):
+            raise MalformedJob()
+        return self.conn.rpush(self.queue_key, json.dumps(job.to_dict()))
+
+    def get_job(self):
+        binary_job = self.conn.lpop(self.queue_key)
+        if binary_job is None:
+            return None
+        encoded_job = binary_job.decode("utf-8")
+        return Job.from_dict(json.loads(encoded_job))
