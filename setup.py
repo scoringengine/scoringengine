d65588a9aa587f16c206581f766bd13af70a0934
Brandon Myers <bmyers@mozilla.com>
2016-11-14 17:03:44 -0600

    Add 4 spaces to setup.py
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 try:
-  from setuptools import setup
+    from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+    from distutils.core import setup
 
 config = {
-  'description': 'Scoring Engine for Red/White/Blue Team Competitions',
-  'author': ['Brandon Myers', 'Rusty Bower'],
-  'url': 'https://github.com/pwnbus/scoring_engine',
-  'download_url': 'https://github.com/pwnbus/scoring_engine/archive/master.zip',
-  'author_email': ['pwnbus@mozilla.com', 'rusty@rustybower.com'],
-  'version': '0.0.1',
-  'install_requires': ['bcrypt',
-                       'codeclimate-test-reporter',
-                       'configparser',
-                       'Flask',
-                       'Flask-Login',
-                       'Flask-SQLAlchemy',
-                       'Flask-WTF',
-                       'pynsive',
-                       'pytest',
-                       'pytest-cov',
-                       'redis',
-                       'sqlalchemy'
-                       ], #dependencies
-  'packages': ['scoring_engine'],
-  'scripts': [],
-  'name': 'scoring_engine'
+    'description': 'Scoring Engine for Red/White/Blue Team Competitions',
+    'author': ['Brandon Myers', 'Rusty Bower'],
+    'url': 'https://github.com/pwnbus/scoring_engine',
+    'download_url': 'https://github.com/pwnbus/scoring_engine/archive/master.zip',
+    'author_email': ['pwnbus@mozilla.com', 'rusty@rustybower.com'],
+    'version': '0.0.1',
+    'install_requires': ['bcrypt',
+                         'codeclimate-test-reporter',
+                         'configparser',
+                         'Flask',
+                         'Flask-Login',
+                         'Flask-SQLAlchemy',
+                         'Flask-WTF',
+                         'pynsive',
+                         'pytest',
+                         'pytest-cov',
+                         'redis',
+                         'sqlalchemy'
+                         ],  # dependencies
+    'packages': ['scoring_engine'],
+    'scripts': [],
+    'name': 'scoring_engine'
 }
 
 setup(**config)
