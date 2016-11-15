b398eb0a5d7043b2c3789c0062f499d29b8a0c9e
Brandon Myers <bmyers@mozilla.com>
2016-11-15 01:48:55 -0600

    Move auth code into auth view
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/web/__init__.py
+++ scoring_engine/web/__init__.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import Flask, g
-from flask_login import LoginManager, current_user
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
+app.secret_key = os.urandom(128)
 
 from scoring_engine.web.views import welcome, scoreboard, overview, services, admin, auth
 
@@ -14,22 +14,3 @@ app.register_blueprint(overview.mod)
 app.register_blueprint(services.mod)
 app.register_blueprint(admin.mod)
 app.register_blueprint(auth.mod)
-
-app.secret_key = os.urandom(128)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-from scoring_engine.models.user import User
-
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-
-@app.before_request
-def get_current_user():
-    g.user = current_user
--- scoring_engine/web/views/auth.py
+++ scoring_engine/web/views/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
 import bcrypt
-from flask import flash, redirect, request, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import flash, redirect, request, url_for, g
+from flask_login import current_user, login_user, logout_user, LoginManager, current_user
 from flask_wtf import FlaskForm
 
 from wtforms import TextField, PasswordField
@@ -11,7 +11,24 @@ from wtforms.validators import InputRequired
 from scoring_engine.db import db
 from scoring_engine.models.user import User
 
+from scoring_engine.web import app
 mod = Blueprint('auth', __name__)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def get_current_user():
+    g.user = current_user
+
 
 # Creating our login form
 class LoginForm(FlaskForm):
