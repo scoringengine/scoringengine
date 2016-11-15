9e85170268c13ce5eb79ec1627fa87181bfa4e2a
Brandon Myers <bmyers@mozilla.com>
2016-11-14 18:33:22 -0600

    Sort of working auth with db
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, g
 from flask_sqlalchemy import SQLAlchemy
 
-from web.views import welcome, scoreboard, overview, services, admin
-# from web.views import auth
+from flask_login import LoginManager, current_user
+
+from web.views import welcome, scoreboard, overview, services, admin, auth
+from web.models.user import User
 
 
 app = Flask(__name__)
@@ -15,9 +17,23 @@ app.register_blueprint(scoreboard.mod)
 app.register_blueprint(overview.mod)
 app.register_blueprint(services.mod)
 app.register_blueprint(admin.mod)
-# app.register_blueprint(auth.mod)
+app.register_blueprint(auth.mod)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.secret_key = os.urandom(128)
 db = SQLAlchemy(app)
 db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def get_current_user():
+    g.user = current_user
--- web/views/auth.py
+++ web/views/auth.py
@@ -2,65 +2,21 @@ from flask import Blueprint, render_template
 
 import bcrypt
 from flask import flash, g, redirect, request, url_for
-from flask_login import current_user, login_user, logout_user, LoginManager
+from flask_login import current_user, login_user, logout_user
 from flask_wtf import FlaskForm
-from sqlalchemy import Boolean, Column, Integer, String
+
 from wtforms import TextField, PasswordField
 from wtforms.validators import InputRequired
 
-from . import db
-from . import app
+from web.database import db_session
+from web.models.user import User
+# from . import app
 
-mod = Blueprint('welcome', __name__)
+mod = Blueprint('auth', __name__)
 
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(20), nullable=False)
-    password = Column(String(50), nullable=False)
-    authenticated = Column(Boolean, default=False)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
-
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-
-@mod.before_request
-def get_current_user():
-    g.user = current_user
-
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
 
 # Creating our login form
 class LoginForm(FlaskForm):
@@ -80,13 +36,13 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        user = db.session.query(User).filter_by(username=username).first()
+        user = db_session.query(User).filter_by(username=username).first()
         if user:
             if bcrypt.hashpw(password.encode('utf-8'), user.password.encode('utf-8')) == user.password:
                 user.authenticated = True
-                current_sessions = db.session.object_session(user)
-                db.session.add(user)
-                db.session.commit()
+                current_sessions = db_session.object_session(user)
+                db_session.add(user)
+                db_session.commit()
                 login_user(user, remember=True)
                 return redirect(url_for("index"))
             else:
