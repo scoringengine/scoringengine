9c480d5a8453ca25710f538d046b83e940a7e03e
Rusty Bower <rbower@digitalocean.com>
2016-11-16 14:00:48 -0600

    Added login_required decorator to protected routes

--- scoring_engine/web/views/services.py
+++ scoring_engine/web/views/services.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 mod = Blueprint('services', __name__)
 
 
 @mod.route('/services')
+@login_required
 def home():
     return render_template('services.html')
 
 
 @mod.route('/service/<id>')
+@login_required
 def service(id):
     return render_template('service.html', service=id)
