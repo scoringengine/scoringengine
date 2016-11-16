58d5a025cb1fc967174f20be7262b11a1ffd8490
Brandon Myers <bmyers@mozilla.com>
2016-11-16 17:17:02 -0600

    Fix spacing in basic_check
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/basic_check.py
+++ scoring_engine/engine/basic_check.py
@@ -8,6 +8,6 @@ class BasicCheck(object):
     def get_ip_address(self):
         ip = [property_obj.value for property_obj in self.properties() if property_obj.name == 'IP Address']
         if ip:
-          return ip[0]
+            return ip[0]
         else:
-          raise LookupError('IP Address property does not exist')
+            raise LookupError('IP Address property does not exist')
