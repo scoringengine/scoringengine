01e2cd1a9e212de66cd9c35ebe4b0758ce5bc5fa
Rusty Bower <rbower@digitalocean.com>
2016-11-16 01:23:25 -0600

    Minor updates to web layout

--- scoring_engine/web/views/auth.py
+++ scoring_engine/web/views/auth.py
@@ -39,7 +39,7 @@ class LoginForm(FlaskForm):
 @mod.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        flash('You are already logged in.')
+        flash('You are already logged in.', 'info')
         return redirect(url_for('admin.home'))
 
     form = LoginForm(request.form)
@@ -50,7 +50,7 @@ def login():
 
         user = db.session.query(User).filter_by(username=username).first()
         if user:
-            if bcrypt.hashpw(password.encode('utf-8'), user.password) == user.password:
+            if bcrypt.hashpw(password.encode('utf-8'), user.password.encode('utf-8')) == user.password.encode('utf-8'):
                 user.authenticated = True
                 db.save(user)
                 current_sessions = db.session.object_session(user)
@@ -73,4 +73,4 @@ def login():
 def logout():
     logout_user()
     flash('You have successfully logged out.', 'success')
-    return redirect(url_for('login'))
+    return redirect(url_for('auth.login'))
