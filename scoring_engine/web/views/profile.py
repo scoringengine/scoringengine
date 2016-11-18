2331deb00e8f207500f034b4cc555d5bd7ff7e5b
Rusty Bower <rusty@rustybower.com>
2016-11-18 00:39:18 -0600

    Removed is_auth as it shadows login_req

--- scoring_engine/web/views/profile.py
+++ scoring_engine/web/views/profile.py
@@ -7,6 +7,4 @@ mod = Blueprint('profile', __name__)
 @mod.route('/profile')
 @login_required
 def home():
-    if current_user.is_authenticated:
-        return render_template('profile.html')
-    return redirect(url_for('auth.unauthorized'))
+    return render_template('profile.html')
