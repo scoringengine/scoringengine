65a3d963ed08a12542d13243b6ecaa64673626e2
Rusty Bower <rusty@rustybower.com>
2016-11-21 03:19:22 -0600

    Added support for auto updating bar graph

--- scoring_engine/web/views/api.py
+++ scoring_engine/web/views/api.py
@@ -129,6 +129,26 @@ def get_checks(id):
     return jsonify(data=data)
 
 
+@mod.route('/api/scoreboard/get_bar_data')
+def scoreboard_get_bar_data():
+    results = Team.get_all_rounds_results()
+    team_data = {}
+
+    team_labels = []
+    team_scores = []
+    scores_colors = []
+    for blue_team in Team.get_all_blue_teams():
+        team_labels.append(blue_team.name)
+        team_scores.append(blue_team.current_score)
+        scores_colors.append(blue_team.rgb_color)
+
+    team_data['labels'] = team_labels
+    team_data['scores'] = team_scores
+    team_data['colors'] = scores_colors
+
+    return jsonify(team_data)
+
+
 @mod.route('/api/scoreboard/get_line_data')
 def scoreboard_get_line_data():
     results = Team.get_all_rounds_results()
