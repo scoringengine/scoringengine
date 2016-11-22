87366156da083d058410537d41ab6de81bade69c
Brandon Myers <bmyers@mozilla.com>
2016-11-21 23:44:17 -0600

    Remove username password check
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/basic_check.py
+++ scoring_engine/engine/basic_check.py
@@ -2,8 +2,8 @@ class BasicCheck(object):
     def __init__(self, service):
         self.service = service
 
-    def properties(self):
-        return self.service.properties
+    def environments(self):
+        return self.service.environments
 
     def get_ip_address(self):
         return self.service.ip_address
