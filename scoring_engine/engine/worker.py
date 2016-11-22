e71b220942d7e0d3095ee62da22f30c8f909c961
Brandon Myers <bmyers@mozilla.com>
2016-11-22 15:54:48 -0600

    Fixup worker
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/worker.py
+++ scoring_engine/engine/worker.py
@@ -1,9 +1,15 @@
 import subprocess
+from functools import partial
 import signal
+import time
 
 from scoring_engine.engine.worker_queue import WorkerQueue
 from scoring_engine.engine.finished_queue import FinishedQueue
 from scoring_engine.engine.config import Config
+
+
+def worker_sigint_handler(signum, frame, worker_obj):
+    worker_obj.shutdown()
 
 
 class Worker(object):
@@ -13,11 +19,11 @@ class Worker(object):
         self.finished_queue = FinishedQueue()
         self.config = Config()
         self.short_circuit = False
-
-        signal.signal(signal.SIGINT, self.shutdown)
-        signal.signal(signal.SIGTERM, self.shutdown)
+        signal.signal(signal.SIGINT, partial(worker_sigint_handler, obj=self))
+        signal.signal(signal.SIGTERM, partial(worker_sigint_handler, obj=self))
 
     def shutdown(self):
+        print("Shutting down worker...")
         self.short_circuit = True
 
     def execute_cmd(self, job, timeout=None):
@@ -42,10 +48,11 @@ class Worker(object):
     def run(self, total_times=-1):
         num_times = 0
         while num_times != total_times and not self.short_circuit:
+            print("Looking for work...")
             num_times += 1
             retrieved_job = self.worker_queue.get_job()
             if retrieved_job is None:
-                # todo block based on get_job
+                time.sleep(2)
                 continue
             updated_job = self.execute_cmd(retrieved_job)
             self.finished_queue.add_job(updated_job)
