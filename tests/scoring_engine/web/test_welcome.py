1caa5e151cc71294fcd9135eec2c1756238fe6d9
Brandon Myers <bmyers@mozilla.com>
2016-11-28 20:40:08 -0600

    Add mock to render_template to get arguments
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- tests/scoring_engine/web/test_welcome.py
+++ tests/scoring_engine/web/test_welcome.py
@@ -3,10 +3,14 @@ from web_test import WebTest
 
 class TestWelcome(WebTest):
 
+    view_name = 'welcome'
+
     def test_home(self):
         resp = self.client.get('/')
+        assert self.mock_obj.call_args == self.build_args('welcome.html')
         assert resp.status_code == 200
 
     def test_home_index(self):
         resp = self.client.get('/index')
+        assert self.mock_obj.call_args == self.build_args('welcome.html')
         assert resp.status_code == 200
--- tests/scoring_engine/web/web_test.py
+++ tests/scoring_engine/web/web_test.py
@@ -1,3 +1,4 @@
+import importlib
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../'))
@@ -5,6 +6,9 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../
 from scoring_engine.web import app
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
 from unit_test import UnitTest
+from mock import MagicMock, call
+
+from flask import render_template as render_template_backup
 
 
 class WebTest(UnitTest):
@@ -13,6 +17,13 @@ class WebTest(UnitTest):
         super(WebTest, self).setup()
         self.app = app
         self.client = self.app.test_client()
+        self.view_module = importlib.import_module('scoring_engine.web.views.' + self.view_name, '*')
+        self.view_module.render_template = MagicMock()
+        self.mock_obj = self.view_module.render_template
+        self.mock_obj.side_effect = lambda *args, **kwargs: render_template_backup(*args, **kwargs)
+
+    def build_args(self, *args, **kwargs):
+        return call(*args, **kwargs)
 
     def verify_auth_required(self, path):
         resp = self.client.get(path)
