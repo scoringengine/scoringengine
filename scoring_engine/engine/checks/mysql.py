803da0df61be655ce7969621760a112fb5d25f96
Jared S <jaredestroud@gmail.com>
2016-12-04 17:33:07 -0500

    Removing for PR

--- scoring_engine/engine/checks/mysql.py
+++
@@ -1,9 +0,0 @@
-from scoring_engine.engine.basic_check import BasicCheck
-
-class MySQLCheck(BasicCheck):
-    name = "MySQL Check"
-    protocol = 'mysql'
-    version = 'ipv4'
-
-    def command(self):
-        return 'mysql -h %s -u %s -p %s %s -e %s ' + %(self.get_ip_address())
