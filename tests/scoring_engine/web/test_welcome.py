e55714ade1bdf7744584e3920923c861f96c1684
Brandon Myers <bmyers@mozilla.com>
2016-12-22 00:42:33 -0600

    Fix welcome view specs
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- tests/scoring_engine/web/test_welcome.py
+++ tests/scoring_engine/web/test_welcome.py
@@ -1,14 +1,23 @@
 from web_test import WebTest
+from collections import OrderedDict
 
 
 class TestWelcome(WebTest):
 
+    def setup(self):
+        super(TestWelcome, self).setup()
+        self.expected_sponsorship_images = OrderedDict()
+        self.expected_sponsorship_images['diamond'] = ['/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg']
+        self.expected_sponsorship_images['platinum'] = ['/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg']
+        self.expected_sponsorship_images['somecustomlevel'] = ['/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg']
+        self.expected_sponsorship_images['gold'] = ['/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg', '/static/images/logo-placeholder.jpg']
+
     def test_home(self):
         resp = self.client.get('/')
-        assert self.mock_obj.call_args == self.build_args('welcome.html')
+        assert self.mock_obj.call_args == self.build_args('welcome.html', sponsorship_images=self.expected_sponsorship_images)
         assert resp.status_code == 200
 
     def test_home_index(self):
         resp = self.client.get('/index')
-        assert self.mock_obj.call_args == self.build_args('welcome.html')
+        assert self.mock_obj.call_args == self.build_args('welcome.html', sponsorship_images=self.expected_sponsorship_images)
         assert resp.status_code == 200
