d08940121f18535fa6b4414c898a67abf2f34bc4
Brandon Myers <bmyers@mozilla.com>
2017-02-23 01:44:57 -0600

    Add improved logging to engine and worker
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/execute_command.py
+++ scoring_engine/engine/execute_command.py
@@ -2,10 +2,13 @@ from scoring_engine.celery_app import celery_app
 from billiard.exceptions import SoftTimeLimitExceeded
 import subprocess
 
+from scoring_engine.logger import logger
+
 
 @celery_app.task(name='execute_command', soft_time_limit=30)
 def execute_command(job):
     output = ""
+    logger.info("Running cmd for " + str(job))
     try:
         cmd_result = subprocess.run(
             job['command'],
---
+++ scoring_engine/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger('scoring_engine')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
