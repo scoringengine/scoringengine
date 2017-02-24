d1dee6bc5e146677f9542ba3825c8c276b6fffc6
Brandon Myers <bmyers@mozilla.com>
2017-02-23 21:32:44 -0600

    Add vhost support to http checks
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/checks/http.py
+++ scoring_engine/engine/checks/http.py
@@ -2,8 +2,8 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 
 class HTTPCheck(BasicCheck):
-    required_properties = ['useragent', 'uri']
+    required_properties = ['useragent', 'vhost', 'uri']
     CMD = 'curl -s -S -4 -v -L  -A "{0}" http://{1}{2}'
 
     def command_format(self, properties):
-        return (properties['useragent'], self.get_ip_address(), properties['uri'])
+        return (properties['useragent'], properties['vhost'], properties['uri'])
