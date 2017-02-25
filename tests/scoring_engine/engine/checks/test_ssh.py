45c98d12e7a200340668918c63aaeefb725da314
Brandon Myers <bmyers@mozilla.com>
2017-02-25 16:45:05 -0600

    Fix pep8 in ssh test
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- tests/scoring_engine/engine/checks/test_ssh.py
+++ tests/scoring_engine/engine/checks/test_ssh.py
@@ -4,9 +4,9 @@ from tests.scoring_engine.engine.checks.check_test import CheckTest
 class TestSSHCheck(CheckTest):
     check_name = 'SSHCheck'
     properties = {
-      'command': 'ls -l'
+        'command': 'ls -l'
     }
     accounts = {
-      'pwnbus': 'pwnbuspass'
+        'pwnbus': 'pwnbuspass'
     }
     cmd = 'expect -c "spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no pwnbus@127.0.0.1 \'ls -l\'; expect \'assword\'; send \'pwnbuspass\r\'; interact\''
