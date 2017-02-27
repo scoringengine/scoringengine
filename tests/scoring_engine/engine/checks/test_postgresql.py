84c1d83b64b44d32b5197066c0115deb70882ab0
Brandon Myers <bmyers@mozilla.com>
2017-02-26 22:22:26 -0600

    Add postgresql check
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

---
+++ tests/scoring_engine/engine/checks/test_postgresql.py
@@ -0,0 +1,13 @@
+from tests.scoring_engine.engine.checks.check_test import CheckTest
+
+
+class TestPOSTGRESQLCheck(CheckTest):
+    check_name = 'POSTGRESQLCheck'
+    properties = {
+        'database': 'testdb',
+        'command': '\d'
+    }
+    accounts = {
+        'pwnbus': 'pwnbuspass'
+    }
+    cmd = "PGPASSWORD=pwnbuspass psql -h 127.0.0.1 -U pwnbus -c '\d' 'testdb'"
