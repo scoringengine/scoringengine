e6bba341deb0bd4de25ec1b9635b0a2a54e01b9f
Brandon Myers <bmyers@mozilla.com>
2017-02-27 18:34:33 -0600

    Add shellescape to check command
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/checks/postgresql.py
+++ scoring_engine/engine/checks/postgresql.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 class POSTGRESQLCheck(BasicCheck):
     required_properties = ['database', 'command']
-    CMD = "PGPASSWORD={0} psql -h {1} -U {2} -c '{3}' '{4}'"
+    CMD = "PGPASSWORD={0} psql -h {1} -U {2} -c {3} {4}"
 
     def command_format(self, properties):
         account = self.get_random_account()
