3fec4855d53a5077762892295582601cc193d068
Brandon Myers <bmyers@mozilla.com>
2017-02-28 21:32:00 -0600

    Update kb test to check for port_num
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- tests/scoring_engine/models/test_kb.py
+++ tests/scoring_engine/models/test_kb.py
@@ -6,12 +6,13 @@ from tests.scoring_engine.unit_test import UnitTest
 class TestKB(UnitTest):
 
     def test_init_property(self):
-        kb = KB(name="task_ids", value="1,2,3,4,5,6")
+        kb = KB(name="task_ids", value="1,2,3,4,5,6", round_num=100)
         assert kb.id is None
         assert kb.name == 'task_ids'
         assert kb.value == '1,2,3,4,5,6'
+        assert kb.round_num == 100
 
     def test_basic_kb(self):
-        kb = KB(name="task_ids", value="1,2,3,4,5,6")
+        kb = KB(name="task_ids", value="1,2,3,4,5,6", round_num=50)
         self.db.save(kb)
         assert kb.id is not None
