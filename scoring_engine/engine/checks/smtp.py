61098ecd6c7bdb8e7115c8c6bc80b4db4472a14f
Brandon Myers <bmyers@mozilla.com>
2017-03-11 17:37:49 -0600

    Fixup smtp and smtps checks
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- scoring_engine/engine/checks/smtp.py
+++ scoring_engine/engine/checks/smtp.py
@@ -2,10 +2,18 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 
 class SMTPCheck(BasicCheck):
-    required_properties = ['domain']
-    CMD = "medusa -R 1 -h {0} -n {1} -u {2} -p {3} -M smtp"
+    required_properties = ['fromuser', 'touser', 'subject', 'body']
+    CMD = 'smtp_check {0} {1} {2} {3} {4} {5} {6} {7}'
 
     def command_format(self, properties):
         account = self.get_random_account()
-        username_with_domain = account.username + '@' + properties['domain']
-        return (self.ip_address, self.port, username_with_domain, account.password)
+        return (
+          account.username,
+          account.password,
+          properties['fromuser'],
+          properties['touser'],
+          properties['subject'],
+          properties['body'],
+          self.ip_address,
+          self.port
+        )
