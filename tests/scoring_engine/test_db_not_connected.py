337e59d5fe7c9f653d3fe08333ba73e0afa804a2
Brandon Myers <bmyers@mozilla.com>
2017-12-07 02:03:01 -0600

    Remove unused test

--- tests/scoring_engine/test_db_not_connected.py
+++
@@ -1,8 +0,0 @@
-from scoring_engine.db_not_connected import DBNotConnected
-
-
-class TestDBNotConnected(object):
-
-    def test_init(self):
-        exception = DBNotConnected()
-        assert str(exception) == "DB is not connected. Must connect first"
