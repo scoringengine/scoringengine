0fd37f8c94f71c91d7e2d9a48b7a2192e194edf2
Brandon Myers <bmyers@mozilla.com>
2017-12-07 02:30:23 -0600

    Configure db to customize isolation level depending on driver

--- scoring_engine/db.py
+++ scoring_engine/db.py
@@ -5,7 +5,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from scoring_engine.engine.config import config
 
 
-engine = create_engine(config.db_uri, isolation_level="READ UNCOMMITTED")
+isolation_level = "READ COMMITTED"
+if 'sqlite' in config.db_uri:
+    # sqlite db does not support transaction based statements
+    # so we have to manually set it to something else
+    isolation_level = "READ UNCOMMITTED"
+
+engine = create_engine(config.db_uri, isolation_level=isolation_level)
 
 session = scoped_session(sessionmaker(bind=engine))
 
