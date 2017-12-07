7a3df63eb832527f9df1b9a974b98cad04ceeca0
Brandon Myers <bmyers@mozilla.com>
2017-12-06 19:46:34 -0600

    Update docker files to wait for bootstrap container

--- docker/wait-for-bootstrap.sh
+++ docker/wait-for-bootstrap.sh
@@ -7,10 +7,10 @@ host="$1"
 shift
 cmd="$@"
 
-while ping $host -c 1 > /dev/null;
+while ping $host -c 1 > /dev/null 2>&1;
 do
   >&2 echo "Bootstrap container is still running...sleeping"
-  sleep 10
+  sleep 30
 done
 
 >&2 echo "Bootstrap container is finished...Starting container"
