3cf14e66eebeb7ed11f45f9b1cbc119576b20513
Brandon Myers <bmyers@mozilla.com>
2017-12-26 20:05:34 -0600

    Modify sleep time in wait for bootstrap

--- docker/wait-for-bootstrap.sh
+++ docker/wait-for-bootstrap.sh
@@ -10,7 +10,7 @@ cmd="$@"
 while ping $host -c 1 > /dev/null 2>&1;
 do
   >&2 echo "Bootstrap container is still running...sleeping"
-  sleep 30
+  sleep 15
 done
 
 >&2 echo "Bootstrap container is finished...Starting container"
