5ff2a8655caa66369733d7c151f36737217498f8
Brandon Myers <bmyers@mozilla.com>
2017-12-26 20:12:57 -0600

    Add monkeypatch for session query problems

--- scoring_engine/db.py
+++ scoring_engine/db.py
@@ -16,3 +16,16 @@ engine = create_engine(config.db_uri, isolation_level=isolation_level)
 session = scoped_session(sessionmaker(bind=engine))
 
 db_salt = bcrypt.gensalt()
+
+
+# This is a monkey patch so that we
+# don't need to commit before every query
+# We got weird results in the web ui when we didn't
+# have this
+def query_monkeypatch(classname):
+    session.commit()
+    return session.orig_query(classname)
+
+
+session.orig_query = session.query
+session.query = query_monkeypatch
