f73dfcbcd04ea7ebd4c1f13ed129ba724af1a354
Brandon Myers <bmyers@mozilla.com>
2017-12-26 21:30:08 -0600

    Improve tests for admin view

--- tests/scoring_engine/web/views/test_admin.py
+++ tests/scoring_engine/web/views/test_admin.py
@@ -1,4 +1,6 @@
 from tests.scoring_engine.web.web_test import WebTest
+from scoring_engine.models.team import Team
+from scoring_engine.models.user import User
 
 
 class TestAdmin(WebTest):
@@ -6,6 +8,17 @@ class TestAdmin(WebTest):
         super(TestAdmin, self).setup()
         self.create_default_user()
         self.create_default_settings()
+
+    def unauthorized_admin_test(self, path):
+        red_team = Team(name="Red Team", color="Red")
+        self.session.add(red_team)
+        user = User(username="testuser_red", password="testpass_red", team=red_team)
+        self.session.add(user)
+        self.session.commit()
+        self.client.login('testuser_red', 'testpass_red')
+        resp = self.client.get(path)
+        assert resp.status_code == 302
+        assert 'unauthorized' in str(resp.data)
 
     def test_auth_required_admin(self):
         self.verify_auth_required('/admin')
@@ -17,12 +30,43 @@ class TestAdmin(WebTest):
         stats_resp = self.auth_and_get_path('/admin/status')
         assert stats_resp.status_code == 200
 
+    def test_auth_bad_auth_status(self):
+        self.unauthorized_admin_test('/admin/status')
+
     def test_auth_required_admin_manage(self):
         self.verify_auth_required('/admin/manage')
         stats_resp = self.auth_and_get_path('/admin/manage')
         assert stats_resp.status_code == 200
 
+    def test_auth_bad_auth_manage(self):
+        self.unauthorized_admin_test('/admin/manage')
+
     def test_auth_required_admin_settings(self):
         self.verify_auth_required('/admin/settings')
         stats_resp = self.auth_and_get_path('/admin/settings')
         assert stats_resp.status_code == 200
+
+    def test_auth_bad_auth_settings(self):
+        self.unauthorized_admin_test('/admin/settings')
+
+    def test_auth_required_admin_progress(self):
+        self.verify_auth_required('/admin/progress')
+        stats_resp = self.auth_and_get_path('/admin/progress')
+        assert stats_resp.status_code == 200
+
+    def test_auth_bad_auth_progress(self):
+        self.unauthorized_admin_test('/admin/progress')
+
+    def test_auth_required_admin_team(self):
+        self.verify_auth_required('/admin/team/1')
+        stats_resp = self.auth_and_get_path('/admin/team/1')
+        assert stats_resp.status_code == 200
+
+    def test_admin_bad_team(self):
+        self.verify_auth_required('/admin/team/20')
+        resp = self.auth_and_get_path('/admin/team/20')
+        assert resp.status_code == 302
+        assert 'unauthorized' in str(resp.data)
+
+    def test_auth_bad_auth_team(self):
+        self.unauthorized_admin_test('/admin/team/3')
