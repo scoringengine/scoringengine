e10eb6ee23083ab3406ed78c39409208739ebc82
Brandon Myers <bmyers@mozilla.com>
2018-02-09 23:32:58 -0600

    Add smb testbed service

--- scoring_engine/engine/checks/smb.py
+++ scoring_engine/engine/checks/smb.py
@@ -2,8 +2,9 @@ from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class SMBCheck(BasicCheck):
-    required_properties = ['commands']
-    CMD = CHECKS_BIN_PATH + '/smb_check.py --host {0} --user {1} --pass {2} --share {3} --file {4} --hash {5}'
+    required_properties = ['share', 'file', 'hash']
+
+    CMD = CHECKS_BIN_PATH + '/smb_check --host {0} --user {1} --pass {2} --share {3} --file {4} --hash {5}'
 
     def command_format(self, properties):
         account = self.get_random_account()
