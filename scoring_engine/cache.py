5004df5dae3ce83f877f98fb7bbbb4f22ce02c6c
Rusty Bower <rusty@rustybower.com>
2018-02-22 23:03:27 -0500

    caching: adding config variables to cache.py

--- scoring_engine/cache.py
+++ scoring_engine/cache.py
@@ -1,8 +1,10 @@
 from flask_caching import Cache
+from scoring_engine.config import config
+
 
 # TODO - Specifying Caching variables or no cache if set
 cache = Cache(config={
     'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_HOST': 'redis',
-    'CACHE_REDIS_PORT': '6379',
+    'CACHE_REDIS_HOST': config.redis_host,
+    'CACHE_REDIS_PORT': config.redis_port,
 })
