35ade5c7389458dd045e0542a37979ffedf19187
Brandon Myers <bmyers@mozilla.com>
2018-02-23 11:25:46 -0600

    Fix team import in service model

--- scoring_engine/models/service.py
+++ scoring_engine/models/service.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from copy import copy
 from scoring_engine.models.base import Base
+from scoring_engine.models.team import Team
 
 
 class Service(Base):
