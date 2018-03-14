4e355836a05bb48c03aa11f3188b321ca9731bcb
Brandon Myers <bmyers@mozilla.com>
2018-03-13 23:33:33 -0500

    Modify rdp check to check exit status

--- scoring_engine/engine/checks/rdp.py
+++ scoring_engine/engine/checks/rdp.py
@@ -1,10 +1,10 @@
-from scoring_engine.engine.basic_check import BasicCheck
+from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class RDPCheck(BasicCheck):
     required_properties = []
 
-    CMD = 'xfreerdp --ignore-certificate --authonly -u {0} -p {1} {2}:{3}'
+    CMD = CHECKS_BIN_PATH + '/rdp_check {0} {1} {2} {3}'
 
     def command_format(self, properties):
         account = self.get_random_account()
--- tests/scoring_engine/engine/checks/test_rdp.py
+++ tests/scoring_engine/engine/checks/test_rdp.py
@@ -1,3 +1,5 @@
+from scoring_engine.engine.basic_check import CHECKS_BIN_PATH
+
 from tests.scoring_engine.engine.checks.check_test import CheckTest
 
 
@@ -6,4 +8,4 @@ class TestRDPCheck(CheckTest):
     accounts = {
         'pwnbus': 'pwnbuspass'
     }
-    cmd = "xfreerdp --ignore-certificate --authonly -u 'pwnbus' -p 'pwnbuspass' '127.0.0.1':1234"
+    cmd = CHECKS_BIN_PATH + "/rdp_check 'pwnbus' 'pwnbuspass' '127.0.0.1' 1234"
