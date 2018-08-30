3626cd412d8d27af148070544421272c19dbdfb5
Rusty Bower <rusty@rustybower.com>
2018-08-29 20:17:48 -0500

    Updating/pinning versions

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ config = {
     'version': version,
     'install_requires': [
         'bcrypt==3.1.3',
-        'celery==4.0.2',
+        'celery==4.1.1',
         'configparser==3.5.0',
-        'Flask==0.12',
-        'Flask-Caching==1.3.3',
+        'Flask==0.12.4',
+        'Flask-Caching==1.4.0',
         'Flask-Login==0.4.0',
         'Flask-SQLAlchemy==2.2',
         'Flask-WTF==0.14.2',
