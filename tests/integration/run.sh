b5c4499b7c433e8d256b87ff098c6524b43a265a
Brandon Myers <bmyers@mozilla.com>
2018-09-21 22:52:10 -0500

    Convert integration tests to using make commands

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -16,23 +16,29 @@ wait_for_container()
 
 # Stop any previous containers from other parts of testing
 echo "Stopping any previous containers"
-docker-compose -f docker-compose.yml -f docker/testbed/docker-compose.yml -f tests/integration/docker-compose.yml -p scoringengine down -v --remove-orphans
+make stop-integration
+make rm-integration
 
 # Build and start the necessary containers
 echo "Building required container environment"
-docker-compose -f docker-compose.yml -f docker/testbed/docker-compose.yml -f tests/integration/docker-compose.yml -p scoringengine build
+make build-integration
 echo "Starting up required container environment"
-docker-compose -f docker-compose.yml -f docker/testbed/docker-compose.yml -f tests/integration/docker-compose.yml -p scoringengine up -d
+make run-integration
 
 # Wait for the bootstrap container to be done (meaning DB is setup)
-wait_for_container "scoringengine_bootstrap_1" 5
+wait_for_container "scoringengine_bootstrap_1" 10
 
 # Modify some settings on the fly so we don't have to wait so long
 echo "Modifying some settings on the fly"
 docker run -it --network=scoringengine_default scoringengine_tester bash -c "python /app/tests/integration/update_settings.py"
 
+# Sleep for a bit so that the engine has time to start up
+# so that we can detect when it stops running
+echo "Sleeping for 20 seconds for the engine to start up"
+sleep 20
+
 # Wait for engine to finish running
-wait_for_container "scoringengine_engine_1" 10
+wait_for_container "scoringengine_engine_1" 15
 
 # Run integration tests against live testbed db
 echo "Running integration tests"
