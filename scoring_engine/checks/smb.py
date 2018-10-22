e30304273e7775fb158dac71f2b980c10292aeb7
Brandon Myers <bmyers@mozilla.com>
2018-10-22 00:06:14 -0500

    Remove unnecessary line in smb check

--- scoring_engine/checks/smb.py
+++ scoring_engine/checks/smb.py
@@ -3,7 +3,6 @@ from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 class SMBCheck(BasicCheck):
     required_properties = ['share', 'file', 'hash']
-
     CMD = CHECKS_BIN_PATH + '/smb_check --host {0} --port {1} --user {2} --pass {3} --share {4} --file {5} --hash {6}'
 
     def command_format(self, properties):
