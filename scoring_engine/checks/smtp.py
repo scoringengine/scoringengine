a4a3eff7943918504deff039425e08c03b8f25a9
Brandon Myers <bmyers@mozilla.com>
2018-10-21 23:51:32 -0500

    Add checks bin to smtp and smtps checks

--- scoring_engine/checks/smtp.py
+++ scoring_engine/checks/smtp.py
@@ -1,9 +1,9 @@
-from scoring_engine.engine.basic_check import BasicCheck
+from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class SMTPCheck(BasicCheck):
     required_properties = ['fromuser', 'touser', 'subject', 'body']
-    CMD = 'smtp_check {0} {1} {2} {3} {4} {5} {6} {7}'
+    CMD = CHECKS_BIN_PATH + '/smtp_check {0} {1} {2} {3} {4} {5} {6} {7}'
 
     def command_format(self, properties):
         account = self.get_random_account()
--- scoring_engine/checks/smtps.py
+++ scoring_engine/checks/smtps.py
@@ -1,9 +1,9 @@
-from scoring_engine.engine.basic_check import BasicCheck
+from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class SMTPSCheck(BasicCheck):
     required_properties = ['fromuser', 'touser', 'subject', 'body']
-    CMD = 'smtps_check {0} {1} {2} {3} {4} {5} {6} {7}'
+    CMD = CHECKS_BIN_PATH + '/smtps_check {0} {1} {2} {3} {4} {5} {6} {7}'
 
     def command_format(self, properties):
         account = self.get_random_account()
