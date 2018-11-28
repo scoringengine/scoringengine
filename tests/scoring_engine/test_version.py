f44857b7a774f97e4715fec09f6de542a54b8d72
Brandon Myers <bmyers@mozilla.com>
2018-11-27 23:29:06 -0600

    Add tests for debug as true and false for version

--- tests/scoring_engine/test_version.py
+++ tests/scoring_engine/test_version.py
@@ -7,13 +7,16 @@ from scoring_engine.version import version
 from tests.scoring_engine.unit_test import UnitTest
 
 
-class MockConfigLoader():
-    def __init__(self):
-        self.mock_keyname = True
-
+class MockConfigTrueDebug():
     @property
     def debug(self):
         return True
+
+
+class MockConfigFalseDebug():
+    @property
+    def debug(self):
+        return False
 
 
 class TestVersion(UnitTest):
@@ -22,13 +25,17 @@ class TestVersion(UnitTest):
         super(TestVersion, self).setup()
         if 'SCORINGENGINE_VERSION' in os.environ:
             del os.environ['SCORINGENGINE_VERSION']
+        self.toggle_debug(False)
 
-    def enable_debug(self):
-        if 'scoring_engine.config_loader' in sys.modules:
-            del sys.modules['scoring_engine.config_loader']
-        import scoring_engine.config_loader
+    def toggle_debug(self, result):
+        if 'scoring_engine.config' in sys.modules:
+            del sys.modules['scoring_engine.config']
+        import scoring_engine.config
 
-        scoring_engine.config_loader.ConfigLoader = MockConfigLoader
+        if result:
+            scoring_engine.config_loader.ConfigLoader = MockConfigTrueDebug
+        else:
+            scoring_engine.config_loader.ConfigLoader = MockConfigFalseDebug
 
         if 'scoring_engine.config' in sys.modules:
             del sys.modules['scoring_engine.config']
@@ -40,20 +47,23 @@ class TestVersion(UnitTest):
 
     def test_environment_var_version(self):
         os.environ['SCORINGENGINE_VERSION'] = 'testver'
-        del sys.modules["scoring_engine.version"]
+        if 'scoring_engine.version' in sys.modules:
+            del sys.modules["scoring_engine.version"]
         from scoring_engine.version import version
         assert version == 'testver'
 
     def test_debug_version(self):
-        self.enable_debug()
-        del sys.modules["scoring_engine.version"]
+        self.toggle_debug(True)
+        if 'scoring_engine.version' in sys.modules:
+            del sys.modules["scoring_engine.version"]
         from scoring_engine.version import version
         # Verify it's a X.X.X-dev
         assert re.search(r'([\d.]+-dev)', version).group(1) == version
 
     def test_debug_env_version(self):
-        self.enable_debug()
+        self.toggle_debug(True)
         os.environ['SCORINGENGINE_VERSION'] = 'abcdefg'
-        del sys.modules["scoring_engine.version"]
+        if 'scoring_engine.version' in sys.modules:
+            del sys.modules["scoring_engine.version"]
         from scoring_engine.version import version
         assert 'abcdefg-dev' == version
