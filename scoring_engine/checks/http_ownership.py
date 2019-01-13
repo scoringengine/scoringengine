ae012c1073991bd3a7585ec75c924eba50adfb3d
Sean Newman <sxn6296@rit.edu>
2019-01-13 15:26:13 -0500

    Add example service configuration to HTTP check
    The example configuration is provided in a docstring-level comment so
    users can get that information in the most relevant area. It will also
    be included later on in competition.yaml

--- scoring_engine/checks/http_ownership.py
+++ scoring_engine/checks/http_ownership.py
@@ -1,3 +1,20 @@
+"""
+Example service configuration:
+- name: KOTH-HTTP
+  check_name: HTTPCheck
+  host: testbed_koth_http
+  port: 80
+  points: 25
+  environments:
+  - matching_content: "^[A-F0-9a-f]{6}$"  # Regex to match RGB hex string
+    properties:
+    - name: useragent
+      value: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)
+    - name: vhost
+      value: testbed_koth_web
+    - name: uri
+      value: "/ownership.html"
+"""
 from scoring_engine.engine.basic_check import BasicCheck
 
 
