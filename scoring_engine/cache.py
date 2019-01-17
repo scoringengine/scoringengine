57725eaa202837211441c12a010a3d7f8df8fc7f
Brandon Myers <bmyers@mozilla.com>
2019-01-17 14:39:05 -0600

    Add redis password to redis cache

--- scoring_engine/cache.py
+++ scoring_engine/cache.py
@@ -6,4 +6,5 @@ cache = Cache(config={
     'CACHE_TYPE': config.cache_type,
     'CACHE_REDIS_HOST': config.redis_host,
     'CACHE_REDIS_PORT': config.redis_port,
+    'CACHE_REDIS_PASSWORD': config.redis_password,
 })
