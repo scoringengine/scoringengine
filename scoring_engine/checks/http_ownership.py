8fab3b97ab5607998e4e9ded96879193156848c9
Sean Newman <sxn6296@rit.edu>
2019-01-30 21:43:13 -0500

    Make it possible to run the http ownership check

--- scoring_engine/checks/http_ownership.py
+++ scoring_engine/checks/http_ownership.py
@@ -15,7 +15,7 @@ Example service configuration:
     - name: uri
       value: "/ownership.html"
 """
-from scoring_engine.engine.basic_check import BasicCheck
+from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class HTTPOwnershipCheck(BasicCheck):
@@ -27,7 +27,7 @@ class HTTPOwnershipCheck(BasicCheck):
     required_properties = ['useragent', 'vhost', 'uri']
     # A separate script is used in order to process the response and find the
     # ownership hash
-    CMD = '/http_ownership_check {0} {1} {2} {3} {4}'
+    CMD = CHECKS_BIN_PATH + '/http_ownership_check {0} {1} {2} {3} {4}'
 
     def command_format(self, properties):
         return (
