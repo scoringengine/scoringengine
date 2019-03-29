614c137e6ac856a4601b5606ae3213ea6e49a3d0
Rusty Bower <rusty@rustybower.com>
2019-03-28 23:11:09 -0400

    Fixing Windows 2003 zone transfer/resolving issue

--- scoring_engine/checks/dns.py
+++ scoring_engine/checks/dns.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 class DNSCheck(BasicCheck):
     required_properties = ['qtype', 'domain']
-    CMD = 'dig @{0} -p {1} -t {2} -q {3}'
+    CMD = 'dig +noedns @{0} -p {1} -t {2} -q {3}'
 
     def command_format(self, properties):
         return (self.host, self.port, properties['qtype'], properties['domain'])
