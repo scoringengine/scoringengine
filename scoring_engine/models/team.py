776687431f89eef5ea36e95e2c1a7de95685bb86
Rusty Bower <rusty@rustybower.com>
2021-11-27 14:16:57 -0600

    database overhaul

--- scoring_engine/models/team.py
+++ scoring_engine/models/team.py
@@ -1,9 +1,13 @@
+import itertools
 import random
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from scoring_engine.models.base import Base
+from scoring_engine.models.check import Check 
 from scoring_engine.models.round import Round
+from scoring_engine.models.service import Service
 from scoring_engine.db import session
 
 
@@ -23,6 +27,28 @@ class Team(Base):
 
     @property
     def current_score(self):
+        return session.query(
+            func.sum(Service.points)
+        ) \
+        .join(Check) \
+        .filter(Service.team_id == self.id) \
+        .filter(Check.result.is_(True)) \
+        .scalar()
+        
+        '''
+        # This will return a tuple with the correct data ala https://github.com/CTFd/CTFd/blob/5599e25fc9771feb7e4a1ed9dd6e65b7da6d22f9/CTFd/utils/scores/__init__.py#L11
+        session.query(
+            Team.name,
+            Team.rgb_color,
+            func.sum(Service.points)
+        ) \
+        .join(Service) \
+        .join(Check) \
+        .filter(Service.team_id) \
+        .filter(Check.result.is_(True)) \
+        .group_by(Service.team_id).all()
+        '''
+
         total_score = 0
         for service in self.services:
             total_score += service.score_earned
@@ -55,6 +81,21 @@ class Team(Base):
     def get_array_of_scores(self, max_round):
         scores = [0]
         overall_score = 0
+
+        round_scores = session.query(
+            func.sum(Service.points),
+        ) \
+        .join(Check) \
+        .join(Round) \
+        .filter(Service.team_id == self.id) \
+        .filter(Check.result.is_(True)) \
+        .filter(Round.number <= max_round) \
+        .group_by(Check.round_id) \
+        .all()
+
+        # Accumulate the scores for each round based on previous round
+        return list(itertools.accumulate([x[0] for x in round_scores]))
+
         for round_num in range(1, max_round + 1):
             current_round_score = 0
             for service in self.services:
@@ -89,9 +130,10 @@ class Team(Base):
         rounds = []
         scores = {}
         blue_teams = session.query(Team).filter(Team.color == 'Blue').all()
-        last_round_obj = session.query(Round).order_by(Round.number.desc()).first()
+        last_round_obj = session.query(func.max(Round.number)).scalar()
         if last_round_obj:
-            last_round = last_round_obj.number
+            last_round = last_round_obj
+            last_round = 10
             for round_num in range(0, last_round + 1):
                 rounds.append("Round " + str(round_num))
 
