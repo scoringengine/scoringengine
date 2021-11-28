84fca1ebfefb96dab2db7ee54ffbea1c9780a99c
Rusty Bower <rusty@rustybower.com>
2021-11-27 21:28:48 -0600

    test_user: formatting and == fix

--- tests/scoring_engine/models/test_user.py
+++ tests/scoring_engine/models/test_user.py
@@ -9,35 +9,34 @@ from tests.scoring_engine.unit_test import UnitTest
 
 
 class TestUser(UnitTest):
-
     def test_init_service(self):
         user = User(username="testuser", password="testpass")
         assert user.id is None
         assert user.username == "testuser"
-        assert user.password == User.generate_hash('testpass')
+        assert user.password == User.generate_hash("testpass")
         assert type(user.password) is str
         assert user.team is None
         assert user.team_id is None
         assert user.is_authenticated is None
         assert user.is_active is True
         assert user.is_anonymous is False
-        assert user.get_username == 'testuser'
+        assert user.get_username == "testuser"
         assert user.get_id() is None
 
     def test_basic_user(self):
-        team = generate_sample_model_tree('Team', self.session)
+        team = generate_sample_model_tree("Team", self.session)
         user = User(username="testuser", password="testpass", team=team)
         self.session.add(user)
         self.session.commit()
-        assert user.id is 1
+        assert user.id == 1
         assert user.username == "testuser"
-        assert user.password == User.generate_hash('testpass')
+        assert user.password == User.generate_hash("testpass")
         assert user.team is team
         assert user.team_id is team.id
         assert user.get_id() == 1
 
     def test_username_unique(self):
-        team = generate_sample_model_tree('Team', self.session)
+        team = generate_sample_model_tree("Team", self.session)
         user1 = User(username="testuser", password="testpass", team=team)
         self.session.add(user1)
         self.session.commit()
@@ -50,7 +49,7 @@ class TestUser(UnitTest):
         team = Team(name="Red Team", color="Red")
         self.session.add(team)
         self.session.commit()
-        user = User(username='testuser', password='testpass', team=team)
+        user = User(username="testuser", password="testpass", team=team)
         self.session.add(user)
         self.session.commit()
         user.is_red_team is True
@@ -61,7 +60,7 @@ class TestUser(UnitTest):
         team = Team(name="White Team", color="White")
         self.session.add(team)
         self.session.commit()
-        user = User(username='testuser', password='testpass', team=team)
+        user = User(username="testuser", password="testpass", team=team)
         self.session.add(user)
         self.session.commit()
         user.is_red_team is False
@@ -72,7 +71,7 @@ class TestUser(UnitTest):
         team = Team(name="Blue Team", color="Blue")
         self.session.add(team)
         self.session.commit()
-        user = User(username='testuser', password='testpass', team=team)
+        user = User(username="testuser", password="testpass", team=team)
         self.session.add(user)
         self.session.commit()
         user.is_red_team is False
