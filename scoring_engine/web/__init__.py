a8c2b88a68c376d07b543bb5e00795a5a706929a
Rusty Bower <rusty@rustybower.com>
2022-03-08 18:58:43 -0600

    notification features

--- scoring_engine/web/__init__.py
+++ scoring_engine/web/__init__.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from flask import Flask
+from easy_profile import EasyProfileMiddleware
 
 from scoring_engine.cache import cache
 from scoring_engine.config import config
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.update(DEBUG=config.debug)
 app.config.update(UPLOAD_FOLDER=config.upload_folder)
 app.secret_key = os.urandom(128)
+app.wsgi_app = EasyProfileMiddleware(app.wsgi_app)
 
 if not config.debug:
     log = logging.getLogger("werkzeug")
@@ -18,26 +20,28 @@ if not config.debug:
 
 from scoring_engine.web.views import (
     welcome,
-    scoreboard,
-    overview,
-    injects,
     services,
-    admin,
-    auth,
+    scoreboard,
     profile,
+    overview,
+    notifications,
+    injects,
+    auth,
     api,
+    admin,
     about,
 )
 
 cache.init_app(app)
 
 app.register_blueprint(welcome.mod)
-app.register_blueprint(scoreboard.mod)
-app.register_blueprint(overview.mod)
-app.register_blueprint(injects.mod)
 app.register_blueprint(services.mod)
-app.register_blueprint(admin.mod)
-app.register_blueprint(auth.mod)
+app.register_blueprint(scoreboard.mod)
 app.register_blueprint(profile.mod)
+app.register_blueprint(overview.mod)
+app.register_blueprint(notifications.mod)
+app.register_blueprint(injects.mod)
+app.register_blueprint(auth.mod)
 app.register_blueprint(api.mod)
+app.register_blueprint(admin.mod)
 app.register_blueprint(about.mod)
