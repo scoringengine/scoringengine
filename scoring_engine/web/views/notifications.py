3dd9ed29f445f2c56626c2e3a3a060c4569d0d1f
Rusty Bower <rusty@rustybower.com>
2022-03-15 20:13:16 -0500

    notification work

--- scoring_engine/web/views/notifications.py
+++ scoring_engine/web/views/notifications.py
@@ -5,10 +5,10 @@ mod = Blueprint("notifications", __name__)
 
 @mod.route("/notifications")
 @mod.route("/notifications/unread")
-def read():
+def unread():
     return render_template("notifications.html")
 
 
 @mod.route("/notifications/read")
-def unread():
+def read():
     return render_template("notifications.html")
