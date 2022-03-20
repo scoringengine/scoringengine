afc5f8dffa96cdedca410465407302910b92153a
Rusty Bower <rusty@rustybower.com>
2022-03-19 22:01:34 -0500

    optimizing get_bar_data

--- scoring_engine/web/views/api/scoreboard.py
+++ scoring_engine/web/views/api/scoreboard.py
@@ -1,25 +1,50 @@
 from flask import jsonify
+from sqlalchemy.sql import func
 
 from scoring_engine.cache import cache
+from scoring_engine.db import session
+from scoring_engine.models.check import Check
+from scoring_engine.models.inject import Inject
+from scoring_engine.models.round import Round
+from scoring_engine.models.service import Service
 from scoring_engine.models.team import Team
+
 from . import mod
 
 
 @mod.route("/api/scoreboard/get_bar_data")
 @cache.memoize()
 def scoreboard_get_bar_data():
+    current_scores = dict(
+        session.query(Service.team_id, func.sum(Service.points))
+        .join(Check)
+        .filter(Check.result.is_(True))
+        .group_by(Service.team_id)
+        .all()
+    )
+
+    inject_scores = dict(
+        session.query(Inject.team_id, func.sum(Inject.score))
+        .filter(Inject.status == "Graded")
+        .group_by(Inject.team_id)
+        .all()
+    )
+
     team_data = {}
     team_labels = []
     team_scores = []
-    inject_scores = []
-    for blue_team in Team.get_all_blue_teams():
+    team_inject_scores = []
+    blue_teams = (
+        session.query(Team).filter(Team.color == "Blue").order_by(Team.name).all()
+    )
+    for blue_team in blue_teams:
         team_labels.append(blue_team.name)
-        team_scores.append(str(blue_team.current_score))
-        inject_scores.append(str(blue_team.current_inject_score))
+        team_scores.append(str(current_scores.get(blue_team.id, 0)))
+        team_inject_scores.append(str(inject_scores.get(blue_team.id, 0)))
 
     team_data["labels"] = team_labels
     team_data["service_scores"] = team_scores
-    team_data["inject_scores"] = inject_scores
+    team_data["inject_scores"] = team_inject_scores
     return jsonify(team_data)
 
 
