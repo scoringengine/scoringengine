e00ae049a280541bed106308b4877af169c8a717
Rusty Bower <rusty@rustybower.com>
2023-01-27 14:14:13 -0500

    initial black team agent

---
+++ scoring_engine/web/views/api/agent.py
@@ -0,0 +1,25 @@
+from flask import jsonify, request
+from flask_login import current_user, login_required
+from sqlalchemy import desc, func
+
+from scoring_engine.cache import cache
+from scoring_engine.db import session
+from scoring_engine.models.check import Check
+from scoring_engine.models.round import Round
+from scoring_engine.models.service import Service
+from scoring_engine.models.team import Team
+
+from . import mod
+
+
+@mod.route("/agent/checkin", methods=["POST"])
+@mod.route("/api/agent/checkin", methods=["POST"])
+def agent_checkin():
+    data = request.get_json()
+    first = data.get('first', False)
+    flags = data.get('flags', [])
+    timestamp = data.get('timestamp', 0)
+
+    print(data)
+    
+    return jsonify({})
