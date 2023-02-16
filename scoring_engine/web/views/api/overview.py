0f38743efa681be86cf7c4b79539d4d4f3792840
Rusty Bower <rusty@rustybower.com>
2023-02-15 21:38:55 -0600

    adding stats and fixing sorting

--- scoring_engine/web/views/api/overview.py
+++ scoring_engine/web/views/api/overview.py
@@ -55,7 +55,7 @@ def overview_data():
     # .all()
 
     team_data = {}
-    teams = session.query(Team).filter(Team.color == "Blue").order_by(Team.name).all()
+    teams = session.query(Team).filter(Team.color == "Blue").order_by(Team.id).all()
     # teams = session.query(Team.id, Team.name).filter(Team.color == 'Blue').order_by(Team.name).all()
     for team in teams:
         query_data = (
@@ -107,7 +107,7 @@ def overview_get_data():
         for team_id in (
             session.query(Team.id)
             .filter(Team.color == "Blue")
-            .order_by(Team.name)
+            .order_by(Team.id)
             .all()
         )
     ]
@@ -130,13 +130,26 @@ def overview_get_data():
         .all()
     )
 
+    num_down_services = dict(
+        session.query(
+            Service.team_id,
+            func.count(Service.team_id),
+        )
+        .join(Check)
+        .join(Round)
+        .filter(Check.result.is_(False))
+        .filter(Round.number == last_round)
+        .group_by(Service.team_id)
+        .all()
+    )
+
     if len(blue_team_ids) > 0:
         # TODO - This could explode if the first team has a different number of services than everyone else
-        total_services = (
-            session.query(Service.id)
-            .filter(Service.team_id == blue_team_ids[0])
-            .count()
-        )
+        # total_services = (
+        #     session.query(Service.id)
+        #     .filter(Service.team_id == blue_team_ids[0])
+        #     .count()
+        # )
 
         team_scores = dict(
             session.query(Service.team_id, func.sum(Service.points).label("score"))
@@ -154,11 +167,14 @@ def overview_get_data():
             zip(team_scores.keys(), ranks)
         )  # {12: 1, 3: 2, 10: 3, 4: 4, 7: 5, 5: 6, 6: 7, 11: 8, 9: 9, 8: 10}
 
+
         for blue_team_id in blue_team_ids:
+            # num_up_services, num_down_services = Round.get_round_stats(blue_team_id, last_round)
             current_scores.append(str(team_scores.get(blue_team_id, 0)))
             current_places.append(str(ranks_dict.get(blue_team_id, 0)))
             service_ratios.append(
-                "{0} / {1}".format(num_up_services.get(blue_team_id, 0), total_services)
+                # '{0} <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> / {1} <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>'.format(num_up_services, num_down_services)  # TODO - I don't like shimming html into this
+                '{0} <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> / {1} <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>'.format(num_up_services.get(blue_team_id, 0), num_down_services.get(blue_team_id, 0))  # TODO - I don't like shimming html into this
             )
 
         data.append(current_scores)
--- scoring_engine/web/views/stats.py
+++ scoring_engine/web/views/stats.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template
+from flask_login import current_user, login_required
 from sqlalchemy import desc
 from sqlalchemy.sql import func
 
 from scoring_engine.cache import cache
 from scoring_engine.config import config
 from scoring_engine.models.check import Check
+from scoring_engine.models.round import Round
+from scoring_engine.models.team import Team
 from scoring_engine.db import session
 
 import pytz
@@ -14,17 +17,31 @@ mod = Blueprint("stats", __name__)
 
 @mod.route("/stats")
 @cache.memoize()
+@login_required
 def home():
+    team = session.query(Team).get(current_user.team.id)
+    if (
+        team is None
+        or not current_user.team == team
+        # or not (current_user.is_blue_team or current_user.is_red_team)
+        or not (current_user.is_blue_team)
+    ):
+        return jsonify({"status": "Unauthorized"}), 403
+
     # TODO - Move this to an API
     stats = []
     res = session.query(Check.round_id, func.min(Check.completed_timestamp), func.max(Check.completed_timestamp)).group_by(Check.round_id).order_by(desc(Check.round_id)).all()
     for row in res:
+        # TODO - There's probably a better way to do this.
+        num_up_services, num_down_services = Round.get_round_stats(current_user.team.id, row[0])
         stats.append(
             {
                 "round_id": row[0],
                 "start_time": row[1].astimezone(pytz.timezone(config.timezone)).strftime('%Y-%m-%d %H:%M:%S %Z'),
                 "end_time": row[2].astimezone(pytz.timezone(config.timezone)).strftime('%Y-%m-%d %H:%M:%S %Z'),
                 "total_seconds": (row[2] - row[1]).seconds,
+                "num_up_services": num_up_services,
+                "num_down_services": num_down_services,
             }
         )
     return render_template("stats.html", stats=stats)
