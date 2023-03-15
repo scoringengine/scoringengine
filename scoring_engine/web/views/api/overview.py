a641763ad2629e59b650a21104f2fe81716132f5
Rusty Bower <rusty@rustybower.com>
2023-03-15 08:39:20 -0500

    fixing overview api performance

--- scoring_engine/web/views/api/overview.py
+++ scoring_engine/web/views/api/overview.py
@@ -5,12 +5,13 @@ from flask import jsonify
 from sqlalchemy import desc
 from sqlalchemy.sql import func
 
-from scoring_engine.cache import cache
-from scoring_engine.db import session
 from scoring_engine.models.check import Check
 from scoring_engine.models.service import Service
 from scoring_engine.models.round import Round
 from scoring_engine.models.team import Team
+
+from scoring_engine.cache import cache
+from scoring_engine.db import session
 
 from . import mod
 
@@ -41,54 +42,33 @@ def overview_get_round_data():
 @mod.route("/api/overview/data")
 @cache.memoize()
 def overview_data():
-    # services_data = session.query(
-    #     Service.team_id,
-    #     Service.check_name,
-    #     Service.host,
-    #     Service.port,
-    #     Check.result,
-    #     func.max(Check.completed_timestamp),
-    # ) \
-    # .join(Check) \
-    # .group_by(Service.team_id) \
-    # .group_by(Service.check_name) \
-    # .all()
+    data = defaultdict(lambda: defaultdict(dict))
 
-    team_data = {}
-    teams = session.query(Team).filter(Team.color == "Blue").order_by(Team.id).all()
-    # teams = session.query(Team.id, Team.name).filter(Team.color == 'Blue').order_by(Team.name).all()
-    for team in teams:
-        query_data = (
+    round_obj = session.query(Round).order_by(Round.number.desc()).first()
+    if round_obj:
+        checks = (
+            session.query(Check.service_id, Check.result)
+            .join(Round)
+            .filter(Round.number == round_obj.number)
+            .subquery()
+        )
+        res = (
             session.query(
-                Service.team_id,
-                Team.name,
-                Service.name,
-                Check.result,
-                func.max(Check.completed_timestamp),
-                Service.host,
-                Service.port,
+                Team.name, Service.name, Service.host, Service.port, checks.c.result
             )
-            .join(Check)
             .join(Team)
-            .filter(Service.team_id == team.id)
-            .group_by(Service.team_id)
-            .group_by(Service.name)
+            .filter(checks.c.service_id == Service.id)
             .all()
         )
 
-        service_data = {
-            x[2]: {"host": x[5], "passing": x[3], "port": x[6]} for x in query_data
-        }
+        for r in res:
+            data[r[0]][r[1]] = {
+                "host": r[2],
+                "port": r[3],
+                "passing": r[4],
+            }
 
-        # service_data = {}
-        # for service in team.services:
-        #     service_data[service.name] = {
-        #         'passing': service.last_check_result(),
-        #         'host': service.host,
-        #         'port': service.port,
-        #     }
-        team_data[team.name] = service_data
-    return jsonify(team_data)
+    return jsonify(data)
 
 
 @mod.route("/api/overview/get_columns")
@@ -105,10 +85,7 @@ def overview_get_data():
     blue_team_ids = [
         team_id[0]
         for team_id in (
-            session.query(Team.id)
-            .filter(Team.color == "Blue")
-            .order_by(Team.id)
-            .all()
+            session.query(Team.id).filter(Team.color == "Blue").order_by(Team.id).all()
         )
     ]
     last_round = Round.get_last_round_num()
@@ -167,14 +144,16 @@ def overview_get_data():
             zip(team_scores.keys(), ranks)
         )  # {12: 1, 3: 2, 10: 3, 4: 4, 7: 5, 5: 6, 6: 7, 11: 8, 9: 9, 8: 10}
 
-
         for blue_team_id in blue_team_ids:
             # num_up_services, num_down_services = Round.get_round_stats(blue_team_id, last_round)
             current_scores.append(str(team_scores.get(blue_team_id, 0)))
             current_places.append(str(ranks_dict.get(blue_team_id, 0)))
             service_ratios.append(
                 # '{0} <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> / {1} <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>'.format(num_up_services, num_down_services)  # TODO - I don't like shimming html into this
-                '{0} <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> / {1} <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>'.format(num_up_services.get(blue_team_id, 0), num_down_services.get(blue_team_id, 0))  # TODO - I don't like shimming html into this
+                '{0} <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> / {1} <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>'.format(
+                    num_up_services.get(blue_team_id, 0),
+                    num_down_services.get(blue_team_id, 0),
+                )  # TODO - I don't like shimming html into this
             )
 
         data.append(current_scores)
