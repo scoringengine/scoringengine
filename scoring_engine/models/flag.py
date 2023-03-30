aa9c78c49d602efbc930819f8b2718deaa715adc
Rusty Bower <rusty@rustybower.com>
2023-03-30 08:13:53 -0500

    Black team agent (#772)
    * Black team agent (#777)
    ---------
    
    Co-authored-by: wumb0 <wumb0@finishyour.beer>

---
+++ scoring_engine/models/flag.py
@@ -0,0 +1,79 @@
+from sqlalchemy import (
+    Column,
+    Enum,
+    Integer,
+    PickleType,
+    DateTime,
+    String,
+    UniqueConstraint,
+    ForeignKey,
+)
+
+# from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+import pytz
+
+import html
+
+import uuid
+
+from scoring_engine.models.base import Base
+from scoring_engine.models.team import Team
+from scoring_engine.config import config
+
+
+import enum
+
+
+class FlagTypeEnum(enum.Enum):
+    file = "file"
+    pipe = "pipe"
+    net = "net"
+    reg = "reg"
+
+
+class Platform(enum.Enum):
+    windows = "win"
+    nix = "nix"
+
+
+class Perm(enum.Enum):
+    user = "user"
+    root = "root"
+
+
+class Flag(Base):
+    __tablename__ = "flags"
+    # id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    type = Column(Enum(FlagTypeEnum), nullable=False)
+    platform = Column(Enum(Platform), nullable=False)
+    data = Column(PickleType, nullable=False)
+    start_time = Column(DateTime(timezone=True), nullable=False)
+    end_time = Column(DateTime(timezone=True), nullable=False)
+    perm = Column(Enum(Perm), nullable=False)
+
+    def as_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "type": self.type.value,
+            "data": self.data,
+            "platform": self.platform.value,
+            "start_time": int(self.start_time.astimezone(pytz.utc).timestamp()),
+            "end_time": int(self.end_time.astimezone(pytz.utc).timestamp()),
+            "perm": self.perm.value,
+        }
+
+
+class Solve(Base):
+    __tablename__ = "flag_solves"
+    __table_args__ = (
+        UniqueConstraint("flag_id", "host", "team_id", name="_flag_host_team_uc"),
+    )
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    host = Column(String(260), nullable=False)
+    flag_id = Column(String(36), ForeignKey("flags.id"))
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    flag = relationship("Flag", backref="solves", lazy="joined")
+    team = relationship("Team", backref="flag_solves", lazy="joined")
---
+++ scoring_engine/web/views/api/agent.py
@@ -0,0 +1,122 @@
+from flask import jsonify, request, abort
+from flask_login import current_user, login_required
+from sqlalchemy import desc, func, exists
+from sqlalchemy.sql.expression import and_
+from datetime import datetime, timedelta
+from typing import Tuple
+
+from scoring_engine.cache import cache
+from scoring_engine.db import session
+from scoring_engine.models.flag import Flag, Solve, Platform
+from scoring_engine.models.check import Check
+from scoring_engine.models.round import Round
+from scoring_engine.models.service import Service
+from scoring_engine.models.team import Team
+from scoring_engine.models.setting import Setting
+
+from . import mod
+
+cache_dict = (
+    {}
+)  # TODO - This is a dev hack. Real users should be using the flask-caching cache to store values
+
+
+@mod.route("/api/agent/flags")
+def agent_show_flags():
+    now = datetime.utcnow()
+    # show upcoming flags a little bit early so red team can plant them
+    # and implants that might stop checking in still get the next set of flags
+    early = now + timedelta(
+        minutes=int(Setting.get_setting("agent_show_flag_early_mins").value)
+    )
+    flags = (
+        session.query(Flag)
+        .filter(and_(early > Flag.start_time, now < Flag.end_time))
+        .all()
+    )
+    return jsonify([flag.as_dict() for flag in flags])
+
+
+@mod.route("/api/agent/checkin")
+def agent_checkin_get():
+    return do_checkin(*get_host_info())
+
+
+@mod.route("/api/agent/checkin", methods=["POST"])
+def agent_checkin_post():
+    data = request.get_json()
+    team, host, platform = get_host_info()
+    flags = data.get("flags", [])
+    flags = session.query(Flag).filter(Flag.id.in_(flags)).all()
+    solves = [
+        Solve(
+            host=host,
+            team=team,
+            flag=flag,
+        )
+        for flag in flags
+    ]
+    session.add_all(solves)
+    return do_checkin(team, host, platform)
+
+
+def get_host_info() -> Tuple[Team, str, Platform]:
+    try:
+        team: Team = session.query(Team).get(int(request.args["team"]))
+        host = request.args["host"]
+        platform = Platform(request.args["plat"])
+    except (ValueError, KeyError):
+        abort(400)
+    if team is None or host is None:
+        abort(400)
+    if not team.is_blue_team:
+        abort(400)
+    return team, host, platform
+
+
+def do_checkin(team, host, platform):
+    now = datetime.utcnow()
+    # show upcoming flags a little bit early so red team can plant them
+    # and implants that might stop checking in still get the next set of flags
+    early = now + timedelta(
+        minutes=int(Setting.get_setting("agent_show_flag_early_mins").value)
+    )
+    # get unsolved flags for this team and host and for this time period
+    flags = (
+        session.query(Flag)
+        .filter(
+            and_(
+                Flag.platform == platform,
+                early > Flag.start_time,
+                now < Flag.end_time,
+            )
+        )
+        .filter(
+            Flag.id.not_in(
+                session.query(Solve.flag_id).filter(
+                    and_(Solve.host == host, Solve.team == team)
+                )
+            )
+        )
+    ).all()
+
+    res = {
+        "flags": [f.as_dict() for f in flags],
+        "config": {
+            "checkin_interval": {
+                "secs": int(Setting.get_setting("agent_checkin_interval_sec").value),
+                "nanos": 0,
+            }
+        },
+        "timestamp": int(datetime.utcnow().timestamp()),
+    }
+
+    # TODO - this is a gross dev hack
+    if cache.config["CACHE_TYPE"] == "null":
+        cache_dict[host] = now
+        # print(cache_dict)
+    else:
+        cache.set(host, res)
+        # print(cache.get(host))
+
+    return jsonify(res)
