b669ee9341c8dcdafaa5e75c8dcf5501977eb6e7
Rusty Bower <rusty@rustybower.com>
2023-05-02 11:15:09 -0500

    bumping flask-wtf to 1.1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ config = {
         'Flask-Caching==1.10.1',
         'Flask-Login==0.6.2',
         'Flask-SQLAlchemy==2.5.1',
-        'Flask-WTF==1.0.1',
+        'Flask-WTF==1.1.1',
         # "itsdangerous==1.1.0",
         # "markupsafe==1.1.1",
         'mysqlclient==2.1.0',
