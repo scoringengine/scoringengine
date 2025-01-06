ca6620b85824456914cb153ee146d57aa1e90135
Rusty Bower <rusty@rustybower.com>
2025-01-06 07:56:37 -0600

    bumping flask-login to 0.6.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ config = {
         "configparser==3.5.0",
         "Flask==3.1.0",
         "Flask-Caching==1.10.1",
-        "Flask-Login==0.6.2",
+        "Flask-Login==0.6.3",
         "Flask-SQLAlchemy==3.1.1",
         "Flask-WTF==1.2.2",
         # "itsdangerous==1.1.0",
