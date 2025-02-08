d8cb6bbf05959c9eefbfb3c05f9b9fd99504d3bc
Jake Smith <jake@metactf.com>
2025-02-07 21:04:33 -0500

    Fix RDP display and optimize LDAP check results

--- scoring_engine/checks/ldap.py
+++ scoring_engine/checks/ldap.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 class LDAPCheck(BasicCheck):
     required_properties = ['domain_base']
-    CMD = "ldapsearch -x -H 'ldap://{0}:{1}' -D 'cn={2},cn=Users,{3}' -w '{4}' -b '{3}' '(objectclass=User)' cn"
+    CMD = "ldapsearch -x -H 'ldap://{0}:{1}' -D 'cn={2},cn=Users,{3}' -w '{4}' -b '{3}' '(objectclass=Domain)' cn"
 
     def command_format(self, properties):
         account = self.get_random_account()
