6687438920b7cd275fab8178571e89e67e2dc6ad
Jake Smith <jake@metactf.com>
2025-02-07 20:27:59 -0500

    Fix ldap required property

--- scoring_engine/checks/ldap.py
+++ scoring_engine/checks/ldap.py
@@ -2,7 +2,7 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 
 class LDAPCheck(BasicCheck):
-    required_properties = ['domain', 'base_dn']
+    required_properties = ['domain_base']
     CMD = "ldapsearch -x -H 'ldap://{0}:{1}' -D 'cn={2},cn=Users,{3}' -w '{4}' -b '{3}' '(objectclass=User)' cn"
 
     def command_format(self, properties):
