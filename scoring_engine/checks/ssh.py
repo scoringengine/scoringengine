c346806de669435b955b1af06aa12253868773f4
Jake Smith <jtsmith282@gmail.com>
2025-03-20 23:44:09 -0400

    Correct quoting on string passing for checks

--- scoring_engine/checks/ssh.py
+++ scoring_engine/checks/ssh.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 class SSHCheck(BasicCheck):
     required_properties = ['commands']
-    CMD = CHECKS_BIN_PATH + "/ssh_check '{0}' '{1}' '{2}' '{3}' {4}"
+    CMD = CHECKS_BIN_PATH + '/ssh_check {0} {1} {2} {3} {4}'
 
     def command_format(self, properties):
         account = self.get_random_account()
--- scoring_engine/checks/webapp_scoringengine.py
+++ scoring_engine/checks/webapp_scoringengine.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 class WebappScoringengineCheck(BasicCheck):
     required_properties = ['scheme', 'basepath']
-    CMD = "pytest --tb=line -s -q -rA --scheme='{0}' --hostip='{1}' --hostport='{2}' --basepath='{3}' --username='{4}' --password='{5}' " + CHECKS_BIN_PATH + "/webapp_scoringengine_check.py"
+    CMD = 'pytest --tb=line -s -q -rA --scheme={0} --hostip={1} --hostport={2} --basepath={3} --username={4} --password={5} ' + CHECKS_BIN_PATH + '/webapp_scoringengine_check.py'
 
     def command_format(self, properties):
         account = self.get_random_account()
