8367d6f4f40191e859c231dbef7e167d7d405005
Rusty Bower <rusty@rustybower.com>
2025-08-12 19:40:45 -0700

    refactor cache update to use existing app context

--- tests/scoring_engine/engine/test_engine.py
+++ tests/scoring_engine/engine/test_engine.py
@@ -1,6 +1,7 @@
 from scoring_engine.engine.engine import Engine
 
 from scoring_engine.models.setting import Setting
+from scoring_engine.web import create_app
 
 from scoring_engine.checks.agent import AgentCheck
 from scoring_engine.checks.icmp import ICMPCheck
@@ -43,6 +44,14 @@ class TestEngine(UnitTest):
         self.session.add(worker_refresh_time_obj)
 
         self.session.commit()
+
+        self.app = create_app()
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+
+    def teardown(self):
+        self.ctx.pop()
+        super(TestEngine, self).teardown()
 
     def test_init(self):
         engine = Engine()
