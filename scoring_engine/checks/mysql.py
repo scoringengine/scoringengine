c5a63f9d49526276afe9948ccfcce6444a9e6200
Rusty Bower <rusty@rustybower.com>
2025-08-24 13:48:33 -0500

    fixing ssl in mysql check

--- scoring_engine/checks/mysql.py
+++ scoring_engine/checks/mysql.py
@@ -3,7 +3,7 @@ from scoring_engine.engine.basic_check import BasicCheck
 
 class MYSQLCheck(BasicCheck):
     required_properties = ['database', 'command']
-    CMD = "mysql -h {0} -P {1} -u {2} -p{3} {4} -e {5}"
+    CMD = "mysql --skip-ssl -h {0} -P {1} -u {2} -p{3} {4} -e {5}"
 
     def command_format(self, properties):
         account = self.get_random_account()
