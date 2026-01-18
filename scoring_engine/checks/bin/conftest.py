15a7059404df25933a3a182c978ec2f5c5addcb5
Jake Smith <3931697+ION28@users.noreply.github.com>
2026-01-18 18:12:27 -0500

    Add functionality for playwright based checks and fix client-side javascript character input restrictions (#1072)
    * Add static hostname to IP mapping option in playwright-based
    
    advanced web checks to handle certain redirection/cookie scenarios
    
    * Update client-side javascript to allow for ^ and $ characters in
    
    user entered strings such as in the matching content field where
    regex expressions are often used

--- scoring_engine/checks/bin/conftest.py
+++ scoring_engine/checks/bin/conftest.py
@@ -3,10 +3,26 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--scheme", action="store", default="")
     parser.addoption("--hostip", action="store", default="")
+    parser.addoption("--hostname", action="store", default="")
     parser.addoption("--hostport", action="store", default="")
     parser.addoption("--basepath", action="store", default="")
     parser.addoption("--username", action="store", default="")
     parser.addoption("--password", action="store", default="")
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args, pytestconfig):
+    host_ip = pytestconfig.getoption("--hostip")
+    host_name = pytestconfig.getoption("--hostname")
+
+    # We only apply the rule if a hostip is actually provided
+    if host_ip and host_name:
+        return {
+            **browser_type_launch_args,
+            "args": [
+                f"--host-resolver-rules=MAP {host_name} {host_ip}"
+            ],
+        }
+    return browser_type_launch_args
 
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args):
